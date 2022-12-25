f04ce9cc135abc0bdc38d2acfb27d6f2cc8d9702
samuel <samuelsguedes@hotmail.com>
2022-12-25 09:33:37 -0300

    Saving

--- code-burger/src/main.jsx
+++ code-burger/src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Login from './page/Login'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "./page/Login";
+import GlobalStyles from "./globalStyles";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <Login/>
-  </React.StrictMode>,
-)
+    <Login />
+    <GlobalStyles />
+  </React.StrictMode>
+);
